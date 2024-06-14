9738a658932f723743962d5f90ebc8296ea6846f
rjojjr <rjojjr91@live.com>
2024-06-14 09:15:09 -0500

    update openconnect script

--- openconnect/install-open-connect-in-gnome.sh
+++ openconnect/install-open-connect-in-gnome.sh
@@ -3,8 +3,13 @@
 # This script is meant to add openconnect
 # as a VPN option in Gnome Network Manager
 
-cd /tmp
-apt install openconnect
+cd /tmp || (mkdir /tmp && cd /tmp)
+sudo apt update
+sudo apt install openconnect
+
+# These versions of the openconnect Network Manager plugin
+# have a userAgent field to mitigate 404 errors from Cisco Anyconnect
+# VPN servers.
 wget http://ftp.us.debian.org/debian/pool/main/n/network-manager-openconnect/network-manager-openconnect_1.2.10-1_amd64.deb
 dpkg -i network-manager-openconnect_1.2.10-1_amd64.deb
 wget http://ftp.us.debian.org/debian/pool/main/n/network-manager-openconnect/network-manager-openconnect-gnome_1.2.10-1_amd64.deb
