b5b6ac6b71289db60f172ef40369848057ec0c28
rjojjr <rjojjr91@live.com>
2024-06-14 10:03:16 -0500

    update conference software scripts

--- conference/install-webex.sh
+++ conference/install-webex.sh
@@ -2,7 +2,10 @@
 
 # Install latest official WebEx app
 
+echo 'Installing official WebEx client...'
 cd /tmp || (mkdir /tmp && cd /tmp)
 wget https://binaries.webex.com/WebexDesktop-Ubuntu-Official-Package/Webex.deb
 dpkg -i Webex.deb
+echo 'Cleaning up ...'
 rm Webex.deb
+echo 'Installed official WebEx client'
--- conference/install-zoom.sh
+++ conference/install-zoom.sh
@@ -3,8 +3,10 @@
 # Install official Zoom app from Zoom server because snap store
 # and Pop Shop versions are broken.
 
-
+echo 'Installing official Zoom client...'
 cd /tmp || (mkdir /tmp && cd /tmp)
 wget https://cdn.zoom.us/prod/6.0.12.5501/zoom_amd64.deb
 dpkg -i zoom_amd64.deb
+echo 'Cleaning up ...'
 rm zoom_amd64.deb
+echo 'Installed official Zoom client'
