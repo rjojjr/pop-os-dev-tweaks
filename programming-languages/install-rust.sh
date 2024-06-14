df501a0ad573b369c1766a83dac0af8c021b8982
rjojjr <rjojjr91@live.com>
2024-06-14 11:56:51 -0500

    add script to install rust

---
+++ programming-languages/install-rust.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Install Rust
+
+echo '*************************************************'
+echo 'Rust Installer'
+echo '*************************************************'
+echo 'Created by [Robert Kirchner JR(rjojjr)](https://github.com/rjojjr)'
+echo ''
+echo 'Please give [this repo](https://github.com/rjojjr/pop-os-dev-tweaks) a star if you find it useful'
+echo '*************************************************'
+
+echo 'Installing Rust...'
+sudo apt install rustup -y
+echo 'Installed Rust'
+echo '*************************************************'
