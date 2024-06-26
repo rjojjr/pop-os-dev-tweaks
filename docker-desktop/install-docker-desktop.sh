a3f13f72f39cfd5ac767e0ab156fc1019bbbc4e2
rjojjr <rjojjr91@live.com>
2024-06-26 16:24:29 -0500

    install docker engine before docker desktop

--- docker-desktop/install-docker-desktop.sh
+++ docker-desktop/install-docker-desktop.sh
@@ -10,6 +10,23 @@ echo ''
 echo 'Please give [this repo](https://github.com/rjojjr/pop-os-dev-tweaks) a star if you find it useful'
 echo '*************************************************'
 
+echo 'Installing Docker Engine'
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 echo 'Installing official Docker Desktop client...'
 cd /tmp || (mkdir /tmp && cd /tmp)
 
