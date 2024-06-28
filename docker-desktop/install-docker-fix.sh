2cb7a9789bd4ef457af2376695487300a9b7d8ae
rjojjr <rjojjr91@live.com>
2024-06-28 12:55:43 -0500

    fix docker creds store fix install script

--- docker-desktop/install-docker-fix.sh
+++ docker-desktop/install-docker-fix.sh
@@ -13,7 +13,7 @@ echo '*************************************************'
 
 echo 'Installing Docker Desktop Config Mitigation...'
 
-cp ./handle-docker-config-corruption.sh /bin/handle-docker-config-corruption.sh
+sudo cp ./handle-docker-config-corruption.sh /bin/handle-docker-config-corruption.sh
 sudo chmod +x /bin/handle-docker-config-corruption.sh
 
 echo '*************************************************'
@@ -23,8 +23,8 @@ cat > ~/.config/autostart/docker-desktop-config-fix.desktop  <<- EOM
 [Desktop Entry]
 Type=Application
 Exec=/bin/handle-docker-config-corruption.sh
-Hidden=false
-NoDisplay=false
+Hidden=true
+NoDisplay=true
 X-GNOME-Autostart-enabled=true
 Name[en_US]=Docker Desktop Config Corruption Fix
 Name=Docker Desktop Config Corruption Fix
